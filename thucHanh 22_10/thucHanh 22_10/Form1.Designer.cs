0d3c4423ac1d2056f260422a01d705241fbf2064
I3achs <161735517+I3achs@users.noreply.github.com>
2024-10-22 09:31:18 +0700

    Add files via upload

---
+++ thucHanh 22_10/thucHanh 22_10/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace thucHanh_22_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtUserName = new TextBox();
+            txtPassword = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // txtUserName
+            // 
+            txtUserName.Location = new Point(319, 189);
+            txtUserName.Name = "txtUserName";
+            txtUserName.Size = new Size(286, 27);
+            txtUserName.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(319, 240);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(286, 27);
+            txtPassword.TabIndex = 1;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(265, 328);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Đăng nhập";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(445, 328);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 3;
+            button2.Text = "Thoát";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(203, 189);
+            label1.Name = "label1";
+            label1.Size = new Size(110, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Tên đăng nhập:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(203, 240);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Mật khẩu:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            label3.Location = new Point(242, 83);
+            label3.Name = "label3";
+            label3.Size = new Size(334, 54);
+            label3.TabIndex = 6;
+            label3.Text = "Form đăng nhập";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUserName);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtUserName;
+        private TextBox txtPassword;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ thucHanh 22_10/thucHanh 22_10/Form1.cs
@@ -0,0 +1,28 @@
+﻿namespace thucHanh_22_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.Text == "admin" && txtPassword.Text == "1234")
+            {
+                
+                MessageBox.Show("Đăng nhập thành công!", "Đăng nhập");
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại.", "Đăng nhập");
+            }
+        }
+    }
+}
